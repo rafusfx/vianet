130aa8aa51678f3ce6bf7db8b73f9a3445d6b7c8
Rafael Rincon <Rafael Rincon>
2016-12-02 19:00:40 -0200

    done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+ 
+
+/*    $.getJSON('data/data.json', function (data) {
+      console.log(data);
+      $.each(data.person, function(){
+
+
+      })
+     
+
+     
+
+      if (items.length) {
+        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var list = $('<ul />').html(content);
+        showData.append(list);
+      }
+    });
+*/
+   
+  });
+
